c6254c0559e9de727e63b56b158e6dd8e7224e79
bogdanmuste <77117160+bogdanmuste@users.noreply.github.com>
2021-04-15 09:51:48 -0700

    inport data from DB

---
+++ public/registru.js
@@ -0,0 +1,43 @@
+
+let allAnimals = [];
+
+function getHtmlRegister(animals) {
+   return animals.map(animal => {
+    return `<tr> 
+    <th></th>
+    <th>${animal.nrCrotal}Nr. Crotal</th>
+    <th>${animal.sex}</th>
+    <th>${animal.birthday}</th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+        
+    </tr>`
+}).join("")
+}
+
+ function showAnimals(animals) {
+    const html = getHtmlRegister(animals);
+
+    const tbody = document.querySelector("#registru tbody");
+    tbody.innerHTML = html;
+}
+ 
+function loadAnimals() {
+    fetch("/registru")
+    .then(r => r.json())
+    .then(animals => {
+        allAnimals = animals;
+        showAnimals(animals);
+    });
+}
+loadAnimals();
