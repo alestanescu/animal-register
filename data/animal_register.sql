b83a816d9b3e9f595d0a592bc2071f160d845b78
bogdanmuste <77117160+bogdanmuste@users.noreply.github.com>
2021-04-22 08:33:21 -0700

    add NEW Animals

--- data/animal_register.sql
+++ data/animal_register.sql
@@ -2,10 +2,10 @@
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 08, 2021 at 06:33 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Gazdă: 127.0.0.1
+-- Timp de generare: apr. 22, 2021 la 05:32 PM
+-- Versiune server: 10.4.17-MariaDB
+-- Versiune PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `animal_register`
+-- Bază de date: `animal_register`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registru`
+-- Structură tabel pentru tabel `registru`
 --
 
 CREATE TABLE `registru` (
@@ -34,47 +34,56 @@ CREATE TABLE `registru` (
   `birthday` date NOT NULL,
   `registrationDate` date NOT NULL,
   `exitDate` date DEFAULT NULL,
-  `motherId` int(11) DEFAULT NULL
+  `motherId` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `registru`
+-- Eliminarea datelor din tabel `registru`
 --
 
-INSERT INTO `registru` (`id`, `nrCrotal`, `sex`, `birthday`, `registrationDate`, `exitDate`, `motherId`) VALUES
-(2, 'RO000', 'M', '2021-04-08', '2021-04-08', NULL, NULL),
-(3, 'RO001', 'F', '2021-04-01', '2021-04-01', NULL, NULL),
-(4, 'RO002', 'M', '2021-04-08', '2021-04-08', NULL, NULL),
-(5, 'RO003', 'F', '2021-04-08', '2021-04-08', NULL, 3),
-(6, 'RO004', 'M', '2021-04-08', '2021-04-08', NULL, 3);
+INSERT INTO `registru` (`id`, `nrCrotal`, `sex`, `birthday`, `registrationDate`, `exitDate`, `motherId`, `description`) VALUES
+(2, 'RO000', 'M', '2016-04-08', '2021-04-08', NULL, NULL, NULL),
+(3, 'RO001', 'F', '2019-04-01', '2021-04-01', NULL, NULL, NULL),
+(4, 'RO002', 'M', '2019-04-08', '2020-02-03', NULL, NULL, NULL),
+(5, 'RO003', 'F', '2021-04-08', '2021-04-08', NULL, 3, NULL),
+(6, 'RO004', 'M', '2021-04-08', '2021-04-08', NULL, 3, NULL),
+(7, 'RO007', 'F', '2016-03-23', '2016-03-23', NULL, NULL, NULL),
+(8, 'RO007', 'F', '2016-03-23', '2016-03-23', NULL, NULL, NULL),
+(9, 'RO008', 'F', '2017-02-21', '2020-01-01', NULL, NULL, NULL),
+(10, 'RO009', 'F', '2017-02-21', '2020-01-01', NULL, NULL, NULL),
+(11, 'RO010', 'M', '2021-02-04', '2021-02-04', NULL, 7, NULL),
+(12, 'RO011', 'F', '2021-02-12', '2021-02-12', NULL, 8, NULL),
+(13, 'RO010', 'M', '2021-02-04', '2021-02-04', NULL, 9, NULL),
+(14, 'RO011', 'F', '2021-02-12', '2021-02-12', NULL, 10, NULL);
 
 --
--- Indexes for dumped tables
+-- Indexuri pentru tabele eliminate
 --
 
 --
--- Indexes for table `registru`
+-- Indexuri pentru tabele `registru`
 --
 ALTER TABLE `registru`
   ADD PRIMARY KEY (`id`),
   ADD KEY `motherId` (`motherId`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pentru tabele eliminate
 --
 
 --
--- AUTO_INCREMENT for table `registru`
+-- AUTO_INCREMENT pentru tabele `registru`
 --
 ALTER TABLE `registru`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- Constraints for dumped tables
+-- Constrângeri pentru tabele eliminate
 --
 
 --
--- Constraints for table `registru`
+-- Constrângeri pentru tabele `registru`
 --
 ALTER TABLE `registru`
   ADD CONSTRAINT `registru_ibfk_1` FOREIGN KEY (`motherId`) REFERENCES `registru` (`id`) ON UPDATE NO ACTION;
